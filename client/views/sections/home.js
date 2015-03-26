bfe1d625e3b22a1a4f4794d5aa4c7c2e2decdb81
kodamirmo <irvingemmanuelgg@gmail.com>
2015-03-25 20:33:17 -0600

    Actualizando los views

---
+++ client/views/sections/home.js
@@ -0,0 +1,37 @@
+Template.home.rendered = function(){
+	//$('.selectpicker').selectpicker();
+};
+
+Template.home.helpers({
+  
+  estados: function(){
+  	return Estado.find();
+  },
+
+  municipios: function(){
+  	console.log('render municipios');
+  	var estado = Session.get('ESTADO');
+  	if(estado)
+  		return Municipio.find({codigoEstado:estado});
+  	return [];
+  },
+
+  showMunicipios: function(){
+  	console.log('render municipios 2');
+  	return Session.get('ESTADO');
+  }
+
+
+});
+
+Template.home.events({
+
+	'change #estadoSelect' : function(e){
+		var val = $('#estadoSelect option:selected').val();
+		if(val){
+			console.log(val);
+			Session.set('ESTADO',val);
+		}
+	}
+
+});
